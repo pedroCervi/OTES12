5ce40e7c6aa3c1788dbce1d10f796094d9ecd643
Pedro Germano Cervi <pedro@magrathealabs.com>
2019-11-23 12:43:41 -0300

    Fixed log client

--- app/clients/logger_client.rb
+++ app/clients/logger_client.rb
@@ -1,29 +1,33 @@
-module Clients::Logger
+module Clients::LoggerClient
   SERVICE_ID = '2'.freeze
-  # TODO: Wait for API development
-  # LOGGER_URL = ''.freeze
+  LOGGER_URL = 'http://localhost:3333'.freeze
+  NAMESPACE = 'logging'.freeze
 
-  def create_error_log(params, errors, action)
-    true
-    # TODO: Wait for API development
-    # HTTParty.post(
-    #   LOGGER_URL,
-    #   params: {
-    #     serviceId: SERVICE_ID,
-    #     description: "Action: #{action}\n Errors: #{errors.to_json}, Params: #{params}"
-    #   }
-    # )
+  def self.create_error_log(params, errors, action)
+    payload = {
+      description: "Action: #{action}\n Errors: #{errors.to_json}, Params: #{params}"
+    }
+
+    HTTParty.post(
+      logger_route,
+      body: payload.to_json,
+      headers: { 'Content-Type' => 'application/json' }
+    )
   end
 
-  def create_info_log(params, person, action)
-    true
-    # TODO: Wait for API development
-    # HTTParty.post(
-    #   LOGGER_URL,
-    #   params: {
-    #     serviceId: SERVICE_ID,
-    #     description: "Action: #{action}\n Person: #{person.to_json}, Params: #{params}"
-    #   }
-    # )
+  def self.create_info_log(params, person, action)
+    payload = {
+      description: "Action: #{action}\n Person: #{person.to_json}, Params: #{params}"
+    }
+
+    HTTParty.post(
+      logger_route,
+      body: payload.to_json,
+      headers: { 'Content-Type' => 'application/json' }
+    )
+  end
+
+  def self.logger_route
+    LOGGER_URL + '/' + NAMESPACE + '/' + SERVICE_ID
   end
 end
